d10f6a8834a51e455480948b810f0b44f14083dc
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-11-17 08:18:37 +0300

    feat: game manager
    + added game manager
    + added global wallet in game manager

---
+++ Assets/_Source/Core/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager main;
+        
+        [SerializeField] private float startCurrency;
+       
+        public float Currency { get; private set; }
+
+        private void Awake()
+        {
+            if (main != null && main != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                main = this;
+                DontDestroyOnLoad(main);
+            }
+
+            if(PlayerPrefs.GetFloat(GlobalKeys.CURRENCY_PP_STRING) > 0)
+            {
+                GetCurrency();
+            }
+            else
+            {
+                Currency = startCurrency;
+                SetCurrency();
+            }
+        }
+        public void AddCurrency(float amount)
+        {
+            Currency += amount;
+            SetCurrency();
+        }
+        public void RemoveCurrency(float amount)
+        {
+            Currency -= amount;
+            SetCurrency();
+        }
+        private void SetCurrency() { PlayerPrefs.SetFloat(GlobalKeys.CURRENCY_PP_STRING, Currency); }
+        private void GetCurrency() { PlayerPrefs.GetFloat(GlobalKeys.CURRENCY_PP_STRING, Currency); }
+    }
+}
---
+++ Assets/_Source/Core/GlobalKeys.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public static class GlobalKeys
+    {
+        /// <summary>
+        /// PP - PlayerPrefs
+        /// </summary>
+        public static string CURRENCY_PP_STRING = "currency_value";
+    }
+}
